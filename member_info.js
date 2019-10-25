af653c006306a9847d873740761d0cb5c9ec6446
dwangman <dennis.wangdot@gmail.com>
2019-10-24 23:05:39 -0700

    adf

--- member_info.js
+++ member_info.js
@@ -1,4 +1,4 @@
 module.exports  = {
-	Charlie1: "Chob 'Megamind' He is a hulking 6 ft monster who loves getting his boobs touched and coconut water",
-	Charlie2: "dominating the world with his forehead"
+	charlie1: "Chob 'Megamind' He is a hulking 6 ft monster who loves getting his boobs touched and coconut water",
+	charlie2: "dominating the world with his forehead"
 };
