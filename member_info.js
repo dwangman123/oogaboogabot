13ac210caa444b9141e4d23a75ceee5a166cf34f
dwangman <dennis.wangdot@gmail.com>
2019-10-26 18:32:19 -0700

    poop

--- member_info.js
+++ member_info.js
@@ -2,13 +2,15 @@ module.exports  = {
 	charlie1: "Charlie \"Megamind\" He is a hulking 6 ft monster who loves getting his pecs touched and coconut water",
 	charlie2: "dominating the world with his forehead",
 	brendan1: "Brendan \"Degenerate\" Lin towers at 3 ft, commonly seen yelling monkey noises while slapping the bois' butts",
-	brendan2: "yelling at his dogshit laptop for 'bad' League teammates",
+	brendan2: "yelling at his dogshit laptop for \"bad\" League teammates",
 	bradley1: "Bradley \"gay\" Liu is an Asian hypebeast who buys fake Supreme from China and spends more time on Tik Tok than breathing",
 	bradley2: "running it down with Brendan while biting his necklace",
 	nikhil1: "Nikhil \"Free poke guy\" Kulkarni is the whitest Indian to exist but still retains his brown side as a stick",
 	nikhil2: "going to speech and debate for no reason",
 	calvin1: "Calvin \"S&D is for intellectuals\" Xie is infamous for his internship at A VENTURE CAPITALIST FIRM and drinking too much coffee",
 	calvin2: "ditching class",
-	frank1: "Frank \"Junchu Yan\" Yan is a Chinese immigrant frequently yells derogatory slurs when playing the dead game Hearthstone.",
-	frank2: "running from the ICE"
+	frank1: "Frank \"Junchu Yan\" Yan is a Chinese immigrant who frequently yells derogatory slurs when playing the dead game Hearthstone.",
+	frank2: "在跑 from the ICE",
+	\\thomas,eric,dennis, evan, dharmil
+
 };
