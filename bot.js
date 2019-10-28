05e992daf45ea76a5bb94fbaa62a526b119edd3f
dwangman <dennis.wangdot@gmail.com>
2019-10-27 21:13:00 -0700

    polished up code

--- bot.js
+++ bot.js
@@ -9,6 +9,7 @@ client.on('ready', () => {
 client.on("message", (message) => {
   var input=message.content.toLowerCase();
 
+  //Dad bot
   if (message.author.bot) {return;}
   if (( input.includes(" i'm ")) || ((input.indexOf("i'm"))==0&&(input.charAt(input.indexOf("i'm")+2)==" "))){
     message.channel.send("Hi" + message.content.substring(input.indexOf(" ", input.indexOf("i'm"))) + ", I'm daddy");
@@ -16,6 +17,8 @@ client.on("message", (message) => {
   if (( input.includes(" im ")) || ((input.indexOf("im"))==0&&(input.charAt(input.indexOf("im")+2)==" "))) {
     message.channel.send("Hi" + message.content.substring(input.indexOf(" ", input.indexOf("im"))) + ", I'm daddy");
   }
+
+  //get info of members
   if (input.includes("!getinfo")) {
       var name = input.substring(input.indexOf("o") + 2);
       var desc=name+"1";
@@ -23,15 +26,25 @@ client.on("message", (message) => {
     message.channel.send("Description: "+info[desc]);
     message.channel.send("Currently ... "+info[curr]);
   }
+
+  //bradley :D
   if (input=="is bradley") {
     message.channel.send("gay? yes");
   }
+
+  //poop machine
   if (input=="!poop"){
     message.channel.send("░░░░░░░░░░░█▀▀░░█░░░░░░\n░░░░░░▄▀▀▀▀░░░░░█▄▄░░░░\n░░░░░░█░█░░░░░░░░░░▐░░░\n░░░░░░▐▐░░░░░░░░░▄░▐░░░\n░░░░░░█░░░░░░░░▄▀▀░▐░░░\n░░░░▄▀░░░░░░░░▐░▄▄▀░░░░\n░░▄▀░░░▐░░░░░█▄▀░▐░░░░░\n░░█░░░▐░░░░░░░░▄░▌░░░░░\n░░░█▄░░▀▄░░░░▄▀█░▌░░░░░\n░░░▌▐▀▀▀░▀▀▀▀░░█░▌░░░░░\n░░▐▌▐▄░░▀▄░░░░░█░█▄▄░░░\n░░░▀▀░▄███▄▄░░░▀▄▄▄▀░░░\n░░░░░░░░░░░░░░░░░░░░░░░");
   }
-  if (input.includes("!penis")){
+
+  //Peepee size machine
+  if (input.includes("!peepee")){
+    if(!message.mentions.members.first()){
+      message.channel.send("Please mention someone on the server");
+    }
+    message.channel.send("peepee size machine");
     user=message.mentions.members.first();
-    message.channel.send(user.displayName+"'s penis:");
+    message.channel.send(user.displayName+"'s peepee:");
     var size=Math.floor(Math.random() * 6) + 1;
     var shaft="";
     for (var i=0;i<size;i++) {
